25da366bba8b12b4a4b15b2039177f7082f8e19a
David Green <davidgreen1993@gmail.com>
2024-04-05 13:22:50 +0100

    removed unnecessary streams in string reversal

--- src/main/java/org/dgreen/projecteuler/utils/StringUtils.java
+++ src/main/java/org/dgreen/projecteuler/utils/StringUtils.java
@@ -1,12 +1,7 @@
 package org.dgreen.projecteuler.utils;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class StringUtils {
     public static String reverse(String str) {
-        return Stream.of(str)
-                .map(it -> new StringBuilder(it).reverse())
-                .collect(Collectors.joining());
+        return new StringBuilder(str).reverse().toString();
     }
 }
